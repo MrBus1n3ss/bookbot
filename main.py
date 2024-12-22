c020d68e3874c205c7be959b77558de07749f545
John Richardson <john.richardson@jvrdev.com>
2024-12-22 14:20:42 -0600

    updated to include a report

--- main.py
+++ main.py
@@ -16,21 +16,43 @@ def count_characters(file_contents):
 
 def count_words(file_contents):
     words = file_contents.split()
-    return words, len(words)
+    return len(words)
 
 
 def open_book(book):
     with open(Path(books_dir / book)) as file:
         file_contents = file.read()
-
     return file_contents
+
+
+def convert_char_count_to_list(char_count_dict):
+    char_count_list = []
+    for k, v in char_count_dict.items():
+        char_count_list.append({'letter': k, 'count': v})
+    return char_count_list
+
+
+def sort_on(dict):
+    return dict['count']
+
+
+def create_report(word_count, char_count_list, book):
+    print(f'--- Begin report of books/{book} ---')
+    print(f'{word_count} words found in the document')
+    for item in char_count_list:
+        letter = item['letter']
+        count = item['count']
+        print(f"The '{letter}' was found {count} times")
+    print('--- End report ---')
 
 
 def main():
     file_contents = open_book('frankenstein.txt')
-    words, word_count = count_words(file_contents)
+    word_count = count_words(file_contents)
     char_count_dict = count_characters(file_contents)
-    print(char_count_dict['r'])
+    char_count_list = convert_char_count_to_list(char_count_dict)
+    char_count_list.sort(reverse=True, key=sort_on)
+    create_report(word_count, char_count_list, 'frankenstein.txt')
 
 
 if __name__ == "__main__":
